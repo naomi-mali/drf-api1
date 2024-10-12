9ba8c2412f5ab5ea0f6de61e8c774bcde471ca98
Timea Mali <derzsi_timea@yahoo.com>
2024-10-12 17:58:17 +0000

    added create function to like serializers to handle duplicates

--- likes/serializers.py
+++ likes/serializers.py
@@ -12,3 +12,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })    
