b47a6fabd96f0f042788c61f1545c5368b914ca0
Timea Mali <derzsi_timea@yahoo.com>
2024-10-13 13:07:31 +0000

    debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -57,7 +57,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
@@ -77,7 +77,7 @@ else:
         r"^https://.*\.gitpod\.io$",
     ]
 
-CORS_ALLOW_CREDENTIALS = True    
+CORS_ALLOW_CREDENTIALS = True
 
 
 
