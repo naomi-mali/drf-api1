16a84170bcae795a9d59fe4cf70d8d0937c6b497
Timea Mali <derzsi_timea@yahoo.com>
2024-10-13 15:01:44 +0000

    added env vars

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -64,17 +65,22 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-naomimali-drfapi1-645ajjtgako.ws.codeinstitute-ide.net',
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
-    'social-api1-ba63b7ae14c7.herokuapp.com',
+    
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
