ecd78bdab8af74437a330836de8989432af8e380
Timea Mali <derzsi_timea@yahoo.com>
2024-10-14 18:44:52 +0000

    added cors val

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -80,7 +80,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
     ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        r"^https://.*\.codeinstitute-ide\.net$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
